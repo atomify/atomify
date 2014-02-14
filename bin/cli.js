75489ddf656d0bd13dd559f8bbf16cf54753cbf6
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2014-02-14 09:51:29 -0500

    New cli supporting nearly all config options thanks to subarg

---
+++ bin/cli.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+var js = require('atomify-js')
+  , css = require('atomify-css')
+  , fs = require('fs')
+  , writer = require('write-to-path')
+  , argv = require('subarg')(process.argv.slice(2))
+
+function write (path) {
+  return function (err, src) {
+    if (path) fs.writeFileSync(path, src, 'utf8');
+  }
+}
+
+var co = argv.c || argv.css
+if (co) {
+  if (typeof co === 'string') co = {entry: co} // -c entry.css
+  if (co._) co.entry = co._[0] // -c [ entry.css ]
+  if (co.e) co.entry = co.e // -c [ -e entry.css ]
+
+  co.variables = co.v || co.variables
+  // TODO: I think we could support plugins if -css checked for strings and require()-ed them
+  //co.plugins = co.p || co.plugins
+  co.output = co.o || co.output
+
+  if (!co.output) {
+    console.error('No output path provided for CSS bundle!')
+    process.exit(1)
+  }
+
+  css(co, writer(co.output, co))
+}
+
+var jo = argv.j || argv.js
+if (jo) {
+  if (typeof jo === 'string') jo = {entry: jo} // -j entry.js
+  if (jo._) jo.entry = jo._[0] // -j [ entry.js ]
+  if (jo.e) jo.entry = jo.e // -j [ -e entry.js ]
+
+  jo.debug = jo.d || jo.debug
+  jo.watch = jo.w || jo.watch
+  jo.transforms = jo.t || jo.transforms
+  jo.output = jo.o || jo.output
+
+  if (!co.output) {
+    console.error('No output path provided for JS bundle!')
+    process.exit(1)
+  }
+
+  js(jo, writer(jo.output, jo))
+}
