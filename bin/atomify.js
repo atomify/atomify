30551019e6202317d40b58dc2d286e74ac4575c1
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2014-02-17 13:07:20 -0500

    Support debug flag for CSS and at top level

--- bin/atomify.js
+++ bin/atomify.js
@@ -3,9 +3,10 @@
 var cli = require('../lib/cli')
   , argv = require('subarg')(process.argv.slice(2), {
     alias: {
-      j: 'js',
-      c: 'css',
-      o: 'output'
+      j: 'js'
+      , c: 'css'
+      , o: 'output'
+      , d: 'debug'
     }
   })
 
