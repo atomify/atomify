f1113737cbe2f9c6edcb67f420b5d08a67c87428
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2014-02-16 21:45:03 -0500

    Basic API tests

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ var js = require('atomify-js')
   , css = require('atomify-css')
   , http = require('./http')
 
-module.exports = function (opts, cb) {
+module.exports = atomify
+
+function atomify (opts, cb) {
   if (opts.css) css(opts.css, callback(cb, 'css'))
   if (opts.js) js(opts.js, callback(cb, 'js'))
-
-  return {
-    css: css
-    , js: js
-    , http: http
-  }
 }
+
+atomify.js = js
+atomify.css = css
+atomify.http = http
 
 function callback (cb, type) {
   return function (err, src) {
