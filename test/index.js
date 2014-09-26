94d616208ecca1e6f11e63cd3d728fc286586f2e
Joey Baker <joey@byjoeybaker.com>
2014-09-26 16:23:57 -0700

    Add tests to make sure no errors are thrown
    also use path.join for multi-platform compatibility

--- test/index.js
+++ test/index.js
@@ -1,88 +1,104 @@
 var test = require('tape')
   , atomify = require('../')
-  , jsf = __dirname + '/fixtures/js/'
-  , cssf = __dirname + '/fixtures/css/'
+  , path = require('path')
+  , jsf = path.join(__dirname, 'fixtures', 'js/')
+  , cssf = path.join(__dirname, 'fixtures', 'css/')
   , fs = require('fs')
   , read = function (file) {
     return fs.readFileSync(file, 'utf8')
   }
 
 var jsCfg = {
-  entry: jsf + 'entry.js'
+  entry: path.join(jsf, '/entry.js')
 }
 
 var cssCfg = {
-  entry: cssf + 'entry.css'
+  entry: path.join(cssf, '/entry.css')
 }
 
 test('js only', function (t) {
-  t.plan(2)
+  t.plan(3)
 
   atomify({js: jsCfg}, function (err, src, type) {
-    t.equal(src, read(jsf + 'bundle.js'))
+    t.error(err, 'does not error')
+    t.equal(src, read(path.join(jsf, '/bundle.js')))
     t.equal(type, 'js')
   })
 })
 
 test('css only', function (t) {
-  t.plan(2)
+  t.plan(3)
 
   atomify({css: cssCfg}, function (err, src, type) {
-    t.equal(src, read(cssf + 'bundle.css'))
+    t.error(err, 'does not error')
+    t.equal(src, read(path.join(cssf, 'bundle.css')))
     t.equal(type, 'css')
   })
 })
 
 test('js and css', function (t) {
-  t.plan(2)
+  t.plan(4)
 
   atomify({js: jsCfg, css: cssCfg}, function (err, src, type) {
-    if (src === read(jsf + 'bundle.js')) {
+    if (src === read(path.join(jsf, 'bundle.js'))) {
+      t.error(err, 'does not error')
       t.equal(type, 'js')
     }
-    if (src === read(cssf + 'bundle.css')) {
+    if (src === read(path.join(cssf, 'bundle.css'))) {
+      t.error(err, 'does not error')
       t.equal(type, 'css')
     }
   })
 })
 
 test('js output does not prevent callback', function (t) {
-  t.plan(2)
+  t.plan(4)
 
   atomify({js: {
     entry: jsCfg.entry
-    , output: jsf + 'bundle-output.js'
+    , output: path.join(jsf, 'bundle-output.js')
   }, css: cssCfg}, function (err, src, type) {
-    if (type === 'css') t.equal(type, 'css')
-    if (type === 'js') t.equal(type, 'js')
+    if (type === 'css'){
+      t.error(err, 'does not error')
+      t.equal(type, 'css')
+    }
+    if (type === 'js'){
+      t.error(err, 'does not error')
+      t.equal(type, 'js')
+    }
   })
 })
 
 test('css output does prevent callback', function (t) {
-  t.plan(1)
+  t.plan(2)
 
   atomify({js: jsCfg, css: {
     entry: cssCfg.entry
-    , output: cssf + 'bundle-output.css'
+    , output: path.join(cssf, 'bundle-output.css')
   }}, function (err, src, type) {
-    t.equal(type, 'js')
+    if (type === 'js'){
+      t.error(err, 'does not error')
+      t.equal(type, 'js')
+    }
   })
 })
 
 test('js property', function (t) {
-  t.plan(2)
+  t.plan(3)
 
   atomify.js(jsCfg.entry, function (err, src, type) {
-    t.equal(src, read(jsf + 'bundle.js'))
+    t.error(err, 'does not error')
+    t.equal(src, read(path.join(jsf, 'bundle.js')))
     t.equal(typeof type, 'undefined')
   })
 })
 
 test('css property', function (t) {
-  t.plan(2)
+  t.plan(3)
 
   atomify.css(cssCfg.entry, function (err, src, type) {
-    t.equal(src, read(cssf + 'bundle.css'))
+    t.error(err, 'does not error')
+    t.equal(src, read(path.join(cssf, 'bundle.css')))
     t.equal(typeof type, 'undefined')
   })
 })
